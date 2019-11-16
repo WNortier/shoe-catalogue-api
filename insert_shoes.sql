bc09e730bc540eb79d89d3ac2d47e8bab12df497
WNortier <wnortier@gmail.com>
2019-11-16 14:09:44 +0200

    experimented with queries

--- insert_shoes.sql
+++ insert_shoes.sql
@@ -1,3 +1,6 @@
 insert into shoes (brand, color, style, size, price, quantity) values ('Yuma', 'Black', 'Sandals', 8, 999, 5);
 insert into shoes (brand, color, style, size, price, quantity) values ('Zonverse', 'Red', 'Sneakers', 10, 1299, 5);
 insert into shoes (brand, color, style, size, price, quantity) values ('Jimmy Woo', 'Metallic', 'High Heels', 8, 1499, 2);
+
+
+shoecatalogue=# select stock.id, brands.brand, colors.color, sizes.size, stock.price, stock.quantity from stock inner join brands on stock.brand_id = brands.id inner join colors on stock.color_id = colors.id inner join sizes on stock.size_id = sizes.id;
--- tables.sql
+++ tables.sql
@@ -11,19 +11,19 @@ create table shoeBrands(
     brands text not null, 
     brands_id int,
     foreign key (brands_id) references shoes(id)
-)
+);
 
 create table shoeColors(
     colors text not null,
     colors_id int,
     foreign key (colors_id) references shoes(id)
-)
+);
 
 create table shoeSizes(
     sizes int not null,
     sizes_id int,
     foreign key (sizes_id) references shoes(id)
-)
+);
 
 create table cart(
     id serial not null primary key,
@@ -36,26 +36,34 @@ create table cart(
 	foreign key (shoes_id) references shoes(id)
 );
 
+create table brands(
+    id serial not null primary key,
+    brand text not null
+);
 
+create table colors(
+    id serial not null primary key,
+    color text not null
+);
 
+create table sizes(
+    id serial not null primary key,
+    sizes text not null
+);
 
-create table shoes(
+create table stock(
     id serial not null primary key,
     brand_id int not null,
     color_id int not null,
-    size int not null,
+    size_id int not null,
     price int not null,
-    quantity int not null
-    foreign key (brand_id) references shoeBrands(id),
-    foreign key (color_id) references shoeColors(id)
+    quantity int not null,
+    foreign key (brand_id) references brands(id),
+    foreign key (color_id) references colors(id),
+    foreign key (size_id) references sizes(id)
 );
 
-create table shoeBrands(
-    id serial not null primary key,
-    brand text not null
-)
-
-create table shoeColors(
-    id serial not null primary key,
-    color text not null,
-)
+insert into stock (brand_id, color_id, size_id, price, quantity) values (1, 1, 1, 999, 5);
+insert into brands (id, brand) values (1, 'Zonverse');
+insert into colors (id, color) values (1, 'Black');
+insert into sizes (id, size) values (1, 6);
