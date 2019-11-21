735aee6d6547579bfe36eb45ac7b7272caecec9c
warwick <warwick.nortier@gmail.com>
2019-11-21 15:57:02 +0200

    implementing bootstrap frontend

--- test/shoe-service-tests.js
+++ test/shoe-service-tests.js
@@ -3,7 +3,7 @@ const ShoeServiceTesting = require("../services/shoes-service");
 
 const pg = require("pg");
 const Pool = pg.Pool;
-const connectionString = process.env.DATABASE_URL || 'postgresql://warwick:pg123@localhost:5432/shoecatalogue_test';
+const connectionString = process.env.DATABASE_URL || 'postgresql://warwick:pg123@localhost:5432/shoecatalogue';
 
 let useSSL = false;
 let local = process.env.LOCAL || false;
